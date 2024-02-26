e6cb9c52eb384f3db0037209c15e87f018183996
Harisali1 <haris.ali@smr-pk.com>
2024-02-26 21:05:38 +0500

    gs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,98 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Image Recognition</title>
+    <meta charset="utf-8"/>
+    <meta name="description"
+          content="Seven admin dashboard live demo. Check out all the features of the admin panel. Light & dark skins. A large number of settings, additional services and widgets."/>
+    <meta name="keywords"
+          content="Seven, bootstrap, bootstrap 5, dmin themes, free admin themes, bootstrap admin, bootstrap dashboard"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta property="og:locale" content="en_US"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:title"
+          content="Seven HTML Pro - Bootstrap 5 HTML Multipurpose Light/Dark Admin Dashboard Theme - Seven HTML Free by KeenThemes"/>
+    <meta property="og:url" content="https://keenthemes.com/products/seven-html-pro"/>
+    <meta property="og:site_name" content="Seven HTML Free by Keenthemes"/>
+    <link rel="canonical" href="https://preview.keenthemes.com/seven-html-pro"/>
+    <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}"/>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700"/>
+    <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css"/>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-37564768-1"></script>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+</head>
+<body id="kt_body" class="auth-bg">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe>
+</noscript>
+<div class="d-flex flex-column flex-root">
+    <style>
+        .auth-page-bg {
+            background-image: url('{{ asset('assets/media/illustrations/sigma-1/14.png') }}');
+        }
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
+        [data-bs-theme="dark"] .auth-page-bg {
+            background-image: url('{{ asset('assets/media/illustrations/sigma-1/14-dark.png') }}');
+        }
+    </style>
+    <div
+        class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed auth-page-bg">
+        <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
+            <a href="" class="mb-12">
+                <img alt="Logo" src="{{ asset('assets/media/logos/logo-default.svg') }}" class="h-60px"/>
+            </a>
+            <div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
+                <div class="text-center mb-10">
+                    <h1 class="text-dark mb-3">
+                        Sign In to Seven HTML Free </h1>
+                    <div class="text-gray-400 fw-semibold fs-4">
+                        New Here?
+                        <a href="{{ route('register') }}" class="link-primary fw-bold">
+                            Create an Account
+                        </a>
+                    </div>
+                </div>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <label class="form-label fs-6 fw-bold text-dark">Email</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                   name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <label class="form-label fw-bold text-dark fs-6 mb-0">Password</label>
+                            <input id="password" type="password"
+                                   class="form-control @error('password') is-invalid @enderror" name="password" required
+                                   autocomplete="current-password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
+                    <div class="row mb-3">
+                        <div class="col-md-12 ">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember"
+                                       id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
@@ -64,10 +100,57 @@
                                 @endif
                             </div>
                         </div>
-                    </form>
-                </div>
+                    </div>
+
+                    <div class="row mb-0">
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-primary col-md-12">
+                                {{ __('Login') }}
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="text-center text-muted text-uppercase fw-bold mb-5">or</div>
+
+                    <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
+                        <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/google-icon.svg') }}"
+                             class="h-20px me-3"/>
+                        Continue with Google
+                    </a>
+
+                    <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
+                        <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/facebook-4.svg') }}"
+                             class="h-20px me-3"/>
+                        Continue with Facebook
+                    </a>
+
+                    <a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
+                        <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/apple-black.svg') }}"
+                             class="theme-light-show h-20px me-3"/>
+                        Continue with Apple
+                    </a>
+                </form>
+            </div>
+        </div>
+
+        <div class="d-flex flex-center flex-column-auto p-10">
+            <div class="d-flex align-items-center fw-semibold fs-6">
+                <a href="https://keenthemes.com" class="text-muted text-hover-primary px-2">About</a>
+                <a href="https://devs.keenthemes.com" class="text-muted text-hover-primary px-2">Support</a>
+                <a href="https://keenthemes.com/products/seven-html-pro" class="text-muted text-hover-primary px-2">
+                    Upgrade To Pro
+                </a>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+
+{{--<script>--}}
+    {{--var hostUrl = "/seven-html-free/assets/";        </script>--}}
+
+<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/custom/authentication/sign-in/general.js') }}"></script>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,178 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Seven HTML Pro - Bootstrap 5 HTML Multipurpose Light/Dark Admin Dashboard Theme - Seven HTML Free by
+        KeenThemes</title>
+    <meta charset="utf-8"/>
+    <meta name="description"
+          content="Seven admin dashboard live demo. Check out all the features of the admin panel. Light & dark skins. A large number of settings, additional services and widgets."/>
+    <meta name="keywords"
+          content="Seven, bootstrap, bootstrap 5, dmin themes, free admin themes, bootstrap admin, bootstrap dashboard"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta property="og:locale" content="en_US"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:title"
+          content="Seven HTML Pro - Bootstrap 5 HTML Multipurpose Light/Dark Admin Dashboard Theme - Seven HTML Free by KeenThemes"/>
+    <meta property="og:url" content="https://keenthemes.com/products/seven-html-pro"/>
+    <meta property="og:site_name" content="Seven HTML Free by Keenthemes"/>
+    <link rel="canonical" href="https://preview.keenthemes.com/seven-html-pro"/>
+    <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700"/>
+    <!--end::Fonts-->
+    <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css"/>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+</head>
+<body id="kt_body" class="auth-bg">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe>
+</noscript>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+<div class="d-flex flex-column flex-root">
+    <style>
+        .auth-page-bg {
+            background-image: url('{{ asset('') }}assets/media/illustrations/sigma-1/14.png');
+        }
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+        [data-bs-theme="dark"] .auth-page-bg {
+            background-image: url('{{ asset('') }}assets/media/illustrations/sigma-1/14-dark.png');
+        }
+    </style>
+    <div
+        class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed auth-page-bg">
+        <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
+            <a href="#" class="mb-12">
+                <img alt="Logo" src="{{ asset('assets/media/logos/logo-default.svg') }}" class="h-60px"/>
+            </a>
+            <div class="w-lg-600px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
+                    <div class="mb-10 text-center">
+                        <h1 class="text-dark mb-3">
+                            Create an Account
+                        </h1>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
+                        <div class="text-gray-400 fw-semibold fs-4">
+                            Already have an account?
+
+                            <a href="" class="link-primary fw-bold">
+                                Sign in here
+                            </a>
+                        </div>
+                        <!--end::Link-->
+                    </div>
+                    <!--end::Heading-->
+
+                    <!--begin::Action-->
+                    <button type="button" class="btn btn-light-primary fw-bold w-100 mb-10">
+                        <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/google-icon.svg') }}"
+                             class="h-20px me-3"/>
+                        Sign in with Google
+                    </button>
+                    <!--end::Action-->
+
+                    <!--begin::Separator-->
+                    <div class="d-flex align-items-center mb-10">
+                        <div class="border-bottom border-gray-300 mw-50 w-100"></div>
+                        <span class="fw-semibold text-gray-400 fs-7 mx-2">OR</span>
+                        <div class="border-bottom border-gray-300 mw-50 w-100"></div>
+                    </div>
+
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="name" class="col-md-4 col-form-label text-left">First Name</label>
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
+                        <div class="col-md-6">
+                            <label for="name" class="col-md-4 col-form-label text-left">Last Name</label>
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                    <div class="row mb-3">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                        <div class="col-md-12">
+                            <label for="email" class="col-md-1 col-form-label text-left">Email</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
+                            @enderror
+
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <label for="password" class="col-md-1 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                    </div>
+                    <div class="d-flex align-items-center mb-3" data-kt-password-meter-control="highlight">
+                        <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                        <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                        <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                        <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"></div>
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
+                    <div class="text-muted">
+                        Use 8 or more characters with a mix of letters, numbers & symbols.
+                    </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <label for="password-confirm" class="col-md-3 col-form-label text-left">{{ __('Confirm Password') }}</label>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
-                    </form>
-                </div>
+                    </div>
+
+                    <div class="fv-row mb-10">
+                        <label class="form-check form-check-custom form-check-solid form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="toc" value="1"/>
+                            <span class="form-check-label fw-semibold text-gray-700 fs-6">
+                I Agree <a href="#" class="ms-1 link-primary">Terms and conditions</a>.
+            </span>
+                        </label>
+                    </div>
+
+                    <div class="row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Register') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/custom/authentication/sign-up/general.js') }}"></script>
+
+</body>
+<!--end::Body-->
+</html>
